b1850d86ac7fdc7243289bc2cde0e416dc958e17
Crypt0s <bryanhalf@gmail.com>
2013-06-18 17:59:51 -0400

    edit for better working.

--- airdrop-immunizer.py
+++ airdrop-immunizer.py
@@ -10,8 +10,34 @@ import sys
 import os
 import re
 from get_mac import *
-from wifiobjects import *
 import pdb
+
+# Ripped this out of the wifiobjects from py, because it was simpler...bad crypt0s.
+class accessPoint:
+    """
+    Access point object
+    """
+    def __init__(self, bssid):
+        self.ruletype = "allow"
+        self.attack = "0"
+        self.type = "access_point"
+        # set first time seen
+        self.bssid = bssid          # bssid of ap
+
+class client:
+    """
+    Client object
+    """
+    def __init__(self, mac):
+        """
+        mac = client mac address in hex   
+        """
+        self.ruletype = "allow"
+        self.attack = "0"
+        self.type = "client"
+        self.mac = mac                # client mac address
+        self.bssid = None             # Bssid of assoicated ap
+
 
 if len(sys.argv) == 1:
     print "Usage:"
@@ -27,19 +53,21 @@ if ':' in sys.argv[1]:
 # If we don't, then it must have been the name of an interface - get it's mac address.
 else:
     mac = get_mac(sys.argv[1])#.replace(':','').decode('hex')
+data = {}
 
 # Create objects out of wifiobjects -- they have all the things we track already, not that we need them (yet, this code will be extended to the rulebuilder)
 immuneap = accessPoint(mac)
 # Add the aspects of the object that are ONLY found in the RULE class
 immuneap.ruletype = "allow"
 immuneap.attack = "0"
-immuneap.type = "access_point"
+immuneap.type = "ap"
 
 # The same as above, but for a client. We create both just because it's easy.
 immuneclient = client(mac)
 immuneclient.ruletype = "allow"
 immuneclient.attack = "0"
 immuneclient.type = "client"
+immuneclient.bssid = mac
 
 filename = sys.argv[2]
 file_exists = os.path.exists(filename)
@@ -55,6 +83,7 @@ if file_exists:
 
 data['ap'+mac] = immuneap.__dict__
 data['client'+mac] = immuneclient.__dict__
+del data['client'+mac]['apObject']
 
 # Remove the Nones and replace with ""
 # Note: does it matter if it's "" vs null when parsed by the rule parser and compared on the match?!
@@ -68,48 +97,3 @@ with open(filename,'w') as conffile:
     print str(sys.argv[1]) + " immunization written to config file at " + filename
 
 # I may have to make my own ap and client wifiobject stubs here in order to keep the peace.    
-#class accessPoint:
-#    """
-#    Access point object
-#    """
-#    def __init__(self, bssid):
-#        self.ruletype = "allow"
-#        self.attack = "0"
-#        self.type = "access_point"
-#        # set first time seen
-#        self.fts = time.time()      # first time object is seen
-#        self.lts = None             # last time object is seen, update on every acccess
-#        self.connectedClients = []  # list of connected clients
-#        self.essid = None           # broadcasted essid
-#        self.bssid = bssid          # bssid of ap
-#        self.hidden = False         # denote if essid is hidden
-#        self.encryption = None      # show encryption level
-#        self.auth = None            # show authentication settings
-#        self.cipher = None          # cipher, either CCMP, TKIP, wep 64/128
-#        self.channel = None         # ap's channel
-#        self.ssidList = []          # rolling list of seen ssid's for this ap
-#        self.oui = self.populateOUI() # lookup the object oui
-#
-#class client:
-#    """
-#    Client object
-#    """
-#    def __init__(self, mac):
-#        """
-#        mac = client mac address in hex   
-#        """
-#        self.ruletype = "allow"
-#        self.attack = "0"
-#        self.type = "client"
-#        self.fts = time.time()        # first time object is seen
-#        self.lts = None               # last time object is seen, update on every access 
-#        self.mac = mac                # client mac address
-#        self.probes = []              # list of probe requests client broadcast
-#        self.assoicated = False       # list if client is associated to an ap
-#        self.bssid = None             # Bssid of assoicated ap
-#        self.wired = False            # not a wired client by default
-#        self.lastBssid = None         # last connected bssid
-#        self.managedFrame = False     # have we seen a managment frame from this client?
-#        self.oui = self.populateOUI() # populate clients oui lookup#
-#        self.apObject = None          # stores reference link to ap# object when connected to bssid
-
